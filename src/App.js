1b11c583e175c402322f96636516fc038c9063b3
Aloïs Hachet <aloishachet@GreyMan.local>
2020-03-22 14:47:19 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let style = {background: 'red'} 
+  return (
+    <div className="App">
+      <div style={style}>HAAAAAAA</div>
+    </div>
+  );
+}
+
+export default App;
